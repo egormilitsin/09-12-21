1f7c968d012e3ba941a79229eabedb5b1bc5f112
ivan_ <ivan_volodin_357@mail.ru>
2021-12-09 19:48:54 +0700

    123

--- 123.py
+++ 123.py
@@ -1,1 +1,38 @@
-print("asdfl;'")
+# Создать класс Книжная полка, где есть два
+# поля - список Книг и тип материала из которого
+# сделана полка ( к примеру дерево). Для создания
+# этого класса нужно создать еще два класса
+# (Книга и Материал). В методах класса Полка нужно
+# сделать метод, который выводит все книги на
+# полке на экран
+
+# class Shelf:
+# def __init__(material):
+# def __init__(material):
+#     self.material = material
+#     books = []
+
+
+class books(object):
+    book1 = "pushkin"
+    book2 = "gogol"
+    book3 = "dostoevsky"
+
+class material(object):
+    material1 = "iron"
+    material2 = "tree"
+    material3 = "black tree"
+
+class polka(books,material):
+    def __init__(self,material):
+        self.material = material
+        books = []
+
+
+polka1 = polka("tree", books)
+print(polka1)
+
+
+
+
+
